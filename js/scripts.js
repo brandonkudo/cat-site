c546d6fe34b5a997a1d8e27241c09533982d956b
brandon kudo and kyle ebersole <brandon@>
2017-09-12 10:53:53 -0700

    Add buttons to change background/text color.

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $("button#dark").click(function() {
+    $("body").removeClass();
+    $("body").addClass("dark");
+  });
+
+  $("button#light").click(function() {
+    $("body").removeClass();
+    $("body").addClass("light");
+  });
+
+
+
+
+});
